c4ab652b04b11553cb6319f918add6b20f9ee892
CaptainCluster <sranta.ville@gmail.com>
2025-02-06 15:06:07 +0200

    Resetting instance, etc

---
+++ src/backend/src/main/java/com/example/backend/result/QuizInstanceResult.java
@@ -0,0 +1,15 @@
+package com.example.backend.result;
+public class QuizInstanceResult 
+{
+  private int lives;
+
+  public QuizInstanceResult(int lives)
+  {
+    this.lives = lives;  
+  }
+
+  public int getLives()
+  {
+    return lives;
+  }
+}
