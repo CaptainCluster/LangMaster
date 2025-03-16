c815540a22a7d86c00fa17ccc87eda6acf70ebed
CaptainCluster <sranta.ville@gmail.com>
2025-03-16 17:46:01 +0200

    New header design

---
+++ src/frontend/src/components/Header/HeaderLink.tsx
@@ -0,0 +1,15 @@
+import { NavLink } from "react-router-dom";
+
+const HeaderLink = ({ url, text }: { url: string; text: string }) => {
+  return (
+    <NavLink
+      to={url}
+      className="text-white mx-14 p-2 text-[32px]"
+      style={{ textDecoration: "none" }}
+    >
+      {text}
+    </NavLink>
+  );
+};
+
+export default HeaderLink;
